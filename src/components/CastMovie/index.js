dbf9ede4c073c3d8c21ae9b6268a5cdebb216a1f
$on Nguyen <55100730+Nguoi0dienyeu@users.noreply.github.com>
2021-10-08 15:19:20 +0700

    done slide cast movie
    done slide cast movie

--- src/components/CastMovie/index.js
+++ src/components/CastMovie/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect} from "react";
 import axios from "axios";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, useParams } from "react-router-dom";
 
 function SimpleSlider() {
 	const [data, setData] = useState([]);
+
 	let { id } = useParams();
 	useEffect(() => {
 		const fetchData = async () => {
@@ -16,28 +17,38 @@ function SimpleSlider() {
 		fetchData();
 	}, [id]);
 
+	  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 7,
+    slidesToScroll: 3,
+    autoPlay:200,
+  };
+
 	return (
 		<section>
-			<div className="cast-crew">
-				<p>Cast and Character</p>
-			</div>
-			<div className="box-people">
-				{data.map((items, index) => (
-					<div className="people_img" key={index}>
+		<div className="cast-crew">
+			<p>Cast/character</p>
+		</div>
+		<Slider {...settings}>
+			{data.map((items, index) => (
+				<div className="box-people">
+					<div className="people_img">
 						<img
-							className="profile"
+							className="profile-m"
+							key={index}
 							src={`https://www.themoviedb.org/t/p/w138_and_h175_face/${items.profile_path}`}
 							alt="cast"
 						/>
 						<div className="cast_txt">
-							<p className="txt">
-								<a href="">{items.original_name}</a>
-							</p>
+							<p className="txt"><a href="">{items.name}</a></p>
 							<p className="txt">{items.character}</p>
 						</div>
 					</div>
-				))}
-			</div>
+				</div>
+			))}
+		</Slider>
 		</section>
 	);
 }
